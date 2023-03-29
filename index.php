2edae74efba6b088e825c884a7456b48fb42cb8d
Galvan Zavala Zavala <>
2023-03-29 14:09:56 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+     echo 'Nombre:'.' Alejandra Galvan Zavala'
+    ?>
+    <br>
+    <?php
+    echo 'Calle:'.'Valle Hermoso'
+    ?>
+    <br>
+    <?php
+    echo 'Numero de Casa:'.'#145'
+    ?>
+    <br>
+    <?php
+    echo 'Municipio:'.'Saltillo'
+    ?>
+    <br>
+    <?php
+    echo 'Colonia:'.'Nuevo Mirasierra'
+    ?>
+    <br>
+         <?php
+    echo 'Telefono:'.' 8448921104'
+    ?>
+    <br>
+
+  </body>
+</html>
+    
+    
+  
---
+++ read.md
@@ -0,0 +1,15 @@
+1.inicio
+2.Leer Nombre
+3.Mostrar 'Alejandra Galvan zavala'
+4.Leer Calle
+5.Mostrar 'Valle Hermoso'
+6.Leer Numero De Casa
+7.Mostrar '#145'
+8.Leer Municipio
+9.Mostrar 'Saltillo
+10.Leer Colonia
+11.Mostar 'Nuevo Mirasierra'
+12.Leer Telefono
+13.Mostar '8448921104'
+14.fin
+
